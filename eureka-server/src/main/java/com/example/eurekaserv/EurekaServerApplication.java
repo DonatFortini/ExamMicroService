8a14a70dbc28046174914a762c46064fe190618f
DonatFortini <donat.fortini00@gmail.com>
2024-12-19 13:55:19 +0100

    feat: ajout d'un serveur Eureka avec configuration initiale et fichiers de wrapper Maven

---
+++ eureka-server/src/main/java/com/example/eurekaserv/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.example.eurekaserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@EnableAdminServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
